a431851f538efafa5c6aad031c091f67a657f0f1
NJVS <neiljonathanv.sagisi+github@gmail.com>
2023-02-22 22:51:03 +0800

    routes update

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,14 +2,14 @@ import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
 import Portfolio from '../pages/Portfolio/Portfolio';
 import Blogs from '../pages/Blogs/Blogs';
-import Services from '../pages/Blogs/Services';
+import Services from '../pages/Services/Services';
 import Contact from '../pages/Contact/Contact';
 
 export const Routes = [
   { path: '/', element: <Home />, },
   { path: '/about', element: <About /> },
   { path: '/portfolio', element: <Portfolio /> },
-  { path: '/blog', element: <Blogs /> },
+  { path: '/blogs', element: <Blogs /> },
   { path: '/services', element: <Services /> },
   { path: '/contact', element: <Contact /> }
 ]
