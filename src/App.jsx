2d636cbd9d3e237d80028b04c570eda3f03952ee
NJVS <neiljonathanv.sagisi+github@gmail.com>
2023-02-22 22:54:34 +0800

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { useRoutes } from "react-router-dom";
 import { Routes } from './routes/routes';
 
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
+import styles from './App.module.scss';
 
 function App() {
   const routes = useRoutes(Routes);
   return (
-    <>
+    <div className={styles.container}>
       <Navbar />
-      {routes}
+      <main>
+        {routes}
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
