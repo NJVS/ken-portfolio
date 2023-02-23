ef911e15e8018a603e67c5e112be47a4f6f3e88e
NJVS <neiljonathanv.sagisi+github@gmail.com>
2023-02-23 22:10:30 +0800

    content data

---
+++ src/data/about.js
@@ -0,0 +1,182 @@
+
+
+const aboutme = {
+  first_name: "Ken",
+  last_name: "Orinion",
+  title: "Photographer",
+  address: "Philippines, Asia, Earth, Solarsystem, Milkyway Galaxy, 82991 Universe",
+  emails: [ "ken@gmail.com", "sample@gmail.com" ], 
+  phones: [ "+63 912 345 6789", "+63 998 765 4321", "(046) 571 0062" ],
+  socials: {
+    facebook: 'https://www.facebook.com',
+    twitter: 'https://www.twitter.com',
+    youtube: 'https://www.youtube.com',
+    instagram: 'https://www.instagram.com',
+    linkedin: 'https://www.linkedin.com',
+  },
+  intro: {
+    title: "Capturing memories through images",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condimentum sit amet libero. Praesent vitae nibh at neque pretium porta. Donec sollicitudin sollicitudin bibendum.",
+    skills: [ // value/100
+      { name: "Photography", value: 90 },
+      { name: "Videography", value: 80 },
+      { name: "Editing", value: 85 }
+    ],
+    work_overview: [
+      { name: "Completed Projects", value: 210 },
+      { name: "Happy Client", value: 350 },
+      { name: "Working Hours", value: 1200 },
+      { name: "Official Partners", value: 45 }
+    ]
+  },
+}
+
+const partners = [
+  {
+    name: "Julius Andre",
+    title: "Photographer",
+    socials: {
+      facebook: 'https://www.facebook.com',
+      twitter: 'https://www.twitter.com',
+      instagram: 'https://www.instagram.com',
+      linkedin: 'https://www.linkedin.com',
+    }
+  },
+  {
+    name: "Luna Ariana",
+    title: "Photographer",
+    socials: {
+      facebook: 'https://www.facebook.com',
+      twitter: 'https://www.twitter.com',
+      instagram: 'https://www.instagram.com',
+      linkedin: 'https://www.linkedin.com',
+    }
+  },
+  {
+    name: "Thomas Clark",
+    title: "Photographer",
+    socials: {
+      facebook: 'https://www.facebook.com',
+      twitter: 'https://www.twitter.com',
+      instagram: 'https://www.instagram.com',
+      linkedin: 'https://www.linkedin.com',
+    }
+  }
+];
+
+const experience = [
+  {
+    title: "Wedding Photographer",
+    year_started: "2018",
+    year_ended: "2019",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condi mentum sit amet libero. Praesent vitae."
+  },
+  {
+    title: "Travel Videographer",
+    year_started: "2019",
+    year_ended: "2020",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condi mentum sit amet libero. Praesent vitae."
+  },
+  {
+    title: "Senior Editor",
+    year_started: "2020",
+    year_ended: "2023",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condi mentum sit amet libero. Praesent vitae."
+  }
+];
+
+const services = [
+  {
+    title: "Commercial",
+    desc: "Lorem ipsum dolor sit amet, consec tetur adipiscing elit. In massa velit."
+  },
+  {
+    title: "Photography",
+    desc: "Lorem ipsum dolor sit amet, consec tetur adipiscing elit. In massa velit."
+  },
+  {
+    title: "Videography",
+    desc: "Lorem ipsum dolor sit amet, consec tetur adipiscing elit. In massa velit."
+  },
+  {
+    title: "Editing",
+    desc: "Lorem ipsum dolor sit amet, consec tetur adipiscing elit. In massa velit."
+  },
+  {
+    title: "Branding",
+    desc: "Lorem ipsum dolor sit amet, consec tetur adipiscing elit. In massa velit."
+  },
+  {
+    title: "Events",
+    desc: "Lorem ipsum dolor sit amet, consec tetur adipiscing elit. In massa velit."
+  },
+]
+
+const faq = [
+  {
+    question: "How do I collaborate with us?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condimentum sit amet libero. Praesent vitae nibh at neque pretium porta. Donec sollicitudin sollicitudin bibendum."
+  },
+  {
+    question: "Can I get soft copy of the shoots after?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condimentum sit amet libero. Praesent vitae nibh at neque pretium porta. Donec sollicitudin sollicitudin bibendum."
+  },
+  {
+    question: "Are you available overseas?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condimentum sit amet libero. Praesent vitae nibh at neque pretium porta. Donec sollicitudin sollicitudin bibendum."
+  },
+  {
+    question: "Do you provide all the props for the concept?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condimentum sit amet libero. Praesent vitae nibh at neque pretium porta. Donec sollicitudin sollicitudin bibendum."
+  },
+  {
+    question: "How long usually it takes to get the finish product?",
+    answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condimentum sit amet libero. Praesent vitae nibh at neque pretium porta. Donec sollicitudin sollicitudin bibendum."
+  }
+];
+
+const testimonials = [
+  { 
+    name: "Joffrey Baratheon", 
+    title: "King of the 7 kingdom",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condi mentum sit amet libero. Praesent vitae. Donec ullam corper enim pellentesque est tempus ornare. Integer ac arcu imperdiet. Aenean lacinia sem arcu, quis posuere justo aliquam at.",
+  },
+  { 
+    name: "Sansa Stark", 
+    title: "Lady of the North",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condi mentum sit amet libero. Praesent vitae. Donec ullam corper enim pellentesque est tempus ornare. Integer ac arcu imperdiet. Aenean lacinia sem arcu, quis posuere justo aliquam at.",
+  },
+  { 
+    name: "John Snow", 
+    title: "Lord Comander of the Night's Watch",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condi mentum sit amet libero. Praesent vitae. Donec ullam corper enim pellentesque est tempus ornare. Integer ac arcu imperdiet. Aenean lacinia sem arcu, quis posuere justo aliquam at.",
+  },
+  { 
+    name: "Tyrion Lannister", 
+    title: "Hand of the King",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In massa velit, dictum ac mattis eu, condi mentum sit amet libero. Praesent vitae. Donec ullam corper enim pellentesque est tempus ornare. Integer ac arcu imperdiet. Aenean lacinia sem arcu, quis posuere justo aliquam at.",
+  },
+];
+
+const pricing = [
+  {
+    title: "Collaboration",
+    price: 0,
+    benefits: ['Brainstorming', 'Find Concept', 'Final Result']
+  },
+  {
+    title: "Basic Plan",
+    price: 250,
+    benefits: ["Concept Included", "8 Hours Shoot", "Free Raw File"]
+  },
+  {
+    title: "Business Plan",
+    price: 450,
+    benefits: ["Concept Included", "8 Hours Shoot", "Free Raw File"]
+  },
+  {
+    title: "Premium Plan",
+    price: 650,
+    benefits: ["Concept Included", "8 Hours Shoot", "Free Raw File"]
+  }
+]
